cabafa2c64ffc743e2614bebc9ff6a14fe14bfdf
delmashajan <delmashajan@gmail.com>
2022-12-06 10:31:51 +0530

    model

--- src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
+++ src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.nest.employeeapp_backend.controller;
 
+import com.nest.employeeapp_backend.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,26 +14,20 @@ public class EmployeeController {
     public String Welcomeemployee(){
         return "welcome employee";
     }
-    @PostMapping("/add")
-    public String Addemployee(){
+    @PostMapping(value = "/add",consumes = "application/json",produces = "application/json")
+    public String Addemployee(@RequestBody Employee e){
+        System.out.println(e.getName().toString());
+        System.out.println(e.getEmpcode());
+        System.out.println(e.getCompanyname().toString());
+        System.out.println(e.getDesignation().toString());
+        System.out.println(e.getSalary());
+        System.out.println(e.getMobileno().toString());
+        System.out.println(e.getUsername().toString());
+        System.out.println(e.getPassword().toString());
+
         return "add employee";
     }
-    @PostMapping("/search")
-    public String Searchemployee(){
-        return "search employee";
-    }
-    @PostMapping("/edit")
-    public String Editemployee(){
-        return "edit employee";
-    }
-    @GetMapping("/view")
-    public String Viewemployee(){
-        return "view employee";
-    }
-    @PostMapping("/delete")
-    public String Deleteemployee(){
-        return "delete employee";
-    }
+
 
 
 }
